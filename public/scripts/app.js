0895cbb76b2da6fbcf96b48a7b13fe5014178765
gitMarcldo <marcldo@users.noreply.github.com>
2021-01-08 17:03:54 -0500

    added template, app, babel

---
+++ public/scripts/app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+console.log("app is running");
+
+//JSX - Javascript XML
+var template = React.createElement(
+  "h1",
+  null,
+  "Indecision App!"
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,7 @@
+console.log("app is running")
+
+//JSX - Javascript XML
+var template = <h1>Indecision App!</h1>;
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
