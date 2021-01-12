c11d6ee4b6a0560e41cd26cb3896f78d37199731
gitMarcldo <marcldo@users.noreply.github.com>
2021-01-12 13:59:20 -0500

    jsx and es6

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,92 @@
 
 console.log("app is running");
 
-//JSX - Javascript XML
+var app = {
+  title: "Indecision App!",
+  subtitle: "Put your life in the hands of a computer",
+  options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Indecision App!"
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    "p",
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? 'Here are your options:' : 'No Options'
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "Item one"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "Item two"
+    )
+  )
 );
+
+var user = {
+  name: 'Marc',
+  age: 31,
+  location: 'Toronto'
+};
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      "p",
+      null,
+      "Location: ",
+      location
+    );
+  }
+}
+
+var templateTwo = React.createElement(
+  "div",
+  null,
+  React.createElement(
+    "h1",
+    null,
+    "Marc Felizardo"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Age: 31"
+  ),
+  getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+var multiplier = {
+  numbers: [1, 2, 3],
+  multiplyBy: 2,
+  multiply: function multiply() {
+    var _this = this;
+
+    return this.numbers.map(function (number) {
+      return _this.multiplyBy * number;
+    });
+  }
+};
+
+console.log(multiplier.multiply());
--- src/app.js
+++ src/app.js
@@ -1,7 +1,54 @@
 console.log("app is running")
 
-//JSX - Javascript XML
-var template = <h1>Indecision App!</h1>;
-var appRoot = document.getElementById('app');
+const app = {
+  title: "Indecision App!",
+  subtitle: "Put your life in the hands of a computer",
+  options: ['One', 'Two']
+}
+
+
+const template = (
+  <div>
+    <h1>{app.title}</h1>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? 'Here are your options:' : 'No Options'}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+const user = {
+  name: 'Marc',
+  age: 31,
+  location: 'Toronto'
+}
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+const templateTwo = (
+  <div>
+    <h1>Marc Felizardo</h1>
+    <p>Age: 31</p>
+    {getLocation(user.location)}
+  </div>
+);
+
+const appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
+
+const multiplier = {
+  numbers: [1, 2, 3],
+  multiplyBy: 2,
+  multiply() {
+    return this.numbers.map((number) => this.multiplyBy * number);
+  }
+
+};
+
+console.log(multiplier.multiply());
