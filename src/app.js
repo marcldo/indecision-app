dd610b8913d6a318f539ab504703a35b65ac07c1
gitMarcldo <marcldo@users.noreply.github.com>
2021-01-18 11:49:09 -0500

    Method Binding and Added state to counter app pt1

--- src/app.js
+++ src/app.js
@@ -27,19 +27,30 @@ class Header extends React.Component {
 }
 
 class Action extends React.Component {
+  handlePick() {
+    console.log('picked')
+  }
   render() {
     return (
       <div>
-        <button>What Should I Do?</button>
+        <button onClick={this.handlePick}>What Should I Do?</button>
       </div>
     );
   }
 }
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
+  handleRemoveAll() {
+    console.log('removed');
+  }
   render() {
     return (
       <div>
+        <button onClick={this.handleRemoveAll}>Remove All</button>
         {
           this.props.options.map((option) => <Option key={option} optionText={option} />)
         }
@@ -57,8 +68,22 @@ class Option extends React.Component {
 }
 
 class AddOption extends React.Component {
+  handleAddOption(e) {
+    e.preventDefault();
+
+    const option = e.target.elements.option.value.trim();
+
+    if (option) {
+      console.log(option)
+    }
+  }
   render() {
-    return <button>Add Option</button>;
+    return (
+      <form onSubmit={this.handleAddOption}>
+        <input type='text' name='option' />
+        <button>Add Option</button>
+      </form>
+    );
   }
 }
 
