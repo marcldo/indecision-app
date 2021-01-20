44bcb02a33da53280e0c561a9fbd418445992a65
gitMarcldo <marcldo@users.noreply.github.com>
2021-01-20 13:49:15 -0500

    default props for Header and IndecisionApp

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ class IndecisionApp extends React.Component {
     this.handlePick = this.handlePick.bind(this);
     this.handleAddOption = this.handleAddOption.bind(this);
     this.state = {
-      options: []
+      options: props.options
     };
   }
   handlePick() {
@@ -34,12 +34,11 @@ class IndecisionApp extends React.Component {
     });
   }
   render() {
-    const title = 'Indecision';
     const subtitle = 'Put your life in the hands of a computer';
 
     return (
       <div>
-        <Header title={title} subtitle={subtitle} />
+        <Header subtitle={subtitle} />
         <Action
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
@@ -56,14 +55,22 @@ class IndecisionApp extends React.Component {
   }
 }
 
+IndecisionApp.defaultProps = {
+  options: []
+}
+
 const Header = (props) => {
   return (
     <div>
       <h1>{props.title}</h1>
-      <h2>{props.subtitle}</h2>
+      {props.subtitle && <h2>{props.subtitle}</h2>}
     </div>
   );
 };
+
+Header.defaultProps = {
+  title: 'Indecision'
+}
 
 const Action = (props) => {
   return (
@@ -91,7 +98,7 @@ const Options = (props) => {
 
 const Option = (props) => {
   return (
-    <div>Option: {props.optionText}</div>
+    <div>{props.optionText}</div>
   );
 };
 
