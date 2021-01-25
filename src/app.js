a1144ff01ed2574e69564618d8715061f130bbd4
gitMarcldo <marcldo@users.noreply.github.com>
2021-01-25 13:54:36 -0500

    babel webpack config

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("app.js running")
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const template = <p>This is JSX from Webpack!</p>;
+
+ReactDOM.render(template, document.getElementById('app'));
+
+
