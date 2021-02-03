7f70025bfc7f68faf4fff32399e26269191c9340
gitMarcldo <marcldo@users.noreply.github.com>
2021-02-03 12:05:10 -0500

    button styling

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Option = (props) => (
   <div>{props.optionText}
     <button
+      className="button button--link"
       onClick={(e) => {
         props.handleDeleteOption(props.optionText);
       }}
